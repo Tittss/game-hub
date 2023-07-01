7e9969683e7358ba54f13021588640e995d780a4
Nguyễn Thành Công <thanhcong3112002@gmail.com>
2023-07-01 15:53:47 +0700

    update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Img, Text } from "@chakra-ui/react";
+import { HStack, Img } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
